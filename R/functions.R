48a0da57932cf2005d270385566e283fcc6720d9
Enrico Schumann <es@enricoschumann.net>
2021-11-20 21:39:57 +0100

    Make various updates
    Export functions 'datasets' and 'fetch_dataset'; remove
    "csv" from names as apparently not all full datasets
    are in CSV format.
    
    process_dataset: Add support for transforming
    datasets into 'zoo' series.
    
    datasets: Return "filename", "description"
    and date of last "update".
    
    Add README.

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,4 @@
-csv_datasets <-
+datasets <-
 function(url = "https://www.bis.org/statistics/full_data_sets.htm", ...) {
 
     u <- url(url)
@@ -6,20 +6,40 @@ function(url = "https://www.bis.org/statistics/full_data_sets.htm", ...) {
     close(u)
     txt <- txt[grep('href=./statistics.*full.*zip', txt)]
 
-    data.frame(filename = gsub(".*(full[^/]+?zip).*", "\\1", txt),
-               description = NA,
-               updated = NA)
+    fn <- gsub(".*(full[^/]+?zip).*", "\\1", txt)
+
+    descr <- gsub("<[^>]*?>", "", txt)
+    descr <- gsub("&nbsp;", " ", descr)
+    descr <- gsub("(.*[)]).*", "\\1", descr)
+
+    upd <- gsub("&nbsp;", " ", txt)
+    upd <- gsub("<[^>]*?>", "", upd)
+    upd <- gsub(paste0(".*?([0-9]+ (",
+                       paste(month.name, collapse = "|"),
+                       ") [0-9]+).*"), "\\1", upd)
+    for (i in 1:12)
+        upd <- sub(month.name[i], i, upd)
+    upd <- as.Date(upd, "%d %m %Y")
+
+
+    data.frame(filename = trimws(fn),
+               description = trimws(descr),
+               updated = upd)
 }
 
 
-fetch_csv <-
+fetch_dataset <-
 function(dest.dir, dataset,
-         bis.url = "https://www.bis.org/statistics/", ...) {
+         bis.url = "https://www.bis.org/statistics/",
+         exdir = tempdir(),
+         return.class = NULL,
+         ...) {
 
 
     if (!dir.exists(dest.dir)) {
         create.dir <- askYesNo(
-            paste(sQuote("dest.dir"), "does not exist. Create it?"))
+            paste(sQuote("dest.dir"), "does not exist. Create it?"),
+            default = FALSE)
         if (!isTRUE(create.dir))
             return(invisible(NULL))
         dir.create(dest.dir, recursive = TRUE)
@@ -41,12 +61,46 @@ function(dest.dir, dataset,
         return(invisible(NULL))
     }
 
-    tmp2 <- unzip(f.path, exdir = tempdir())
-    txt <- read.table(tmp2, header = TRUE, sep = ",",
-                      stringsAsFactors = FALSE,
-                      check.names = FALSE)
-    file.remove(tmp2)
+    txt <- process_dataset(f.path,
+                           exdir = exdir,
+                           return.class = return.class)
 
     ## TODO apply filespecific parsing
     txt
+}
+
+process_dataset <- function(f.path, exdir, return.class, ...) {
+
+    tmp <- unzip(f.path, exdir = exdir)
+    on.exit(file.remove(tmp))
+    txt <- read.table(tmp, header = TRUE, sep = ",",
+                      stringsAsFactors = FALSE,
+                      check.names = FALSE)
+
+    if (is.null(return.class))
+        return(txt)
+
+    if (return.class == "zoo") {
+        if (!requireNamespace("zoo")) {
+            warning("package ", sQuote("zoo"), " not available")
+            return(txt)
+        }
+
+        if (grepl("full_webstats_credit_gap_dataflow_csv.zip",
+                  basename(f.path), fixed = TRUE) ||
+            grepl("full_bis_total_credit_csv.zip",
+                  basename(f.path), fixed = TRUE)) {
+
+            ## FIXME grep for first date?
+            j <- which(colnames(txt) == "Time Period")
+            ans <- t(txt[, -seq_len(j)])
+            ans <- zoo::zoo(ans,
+                            zoo::as.yearqtr(rownames(ans),
+                                            format = "%Y-Q%q"))
+            attr(ans, "headers") <- t(txt[, seq_len(j)])
+            colnames(ans) <- colnames(attr(ans, "headers")) <-
+                txt[["Time Period"]]
+        }
+    }
+    ans
 }
